6d70fc0d25e4572264de3db706ad87d99e97ed66
Astik Yadav <astikyadav4@gmail.com>
2025-12-22 10:32:08 +0530

    Bug fixed in python service

--- pythonService/rating-service/main.py
+++ pythonService/rating-service/main.py
@@ -1,6 +1,9 @@
 import py_eureka_client.eureka_client as eureka_client
 from fastapi import FastAPI
 from controllers.rating_controller import router as rating_router
+import os
+
+PORT = int(os.getenv("PORT", 8083))
 
 app = FastAPI(title="Rating Service")
 
@@ -10,9 +13,8 @@ async def startup_event():
     await eureka_client.init_async(
         eureka_server="http://localhost:8761/eureka",
         app_name="RATING-SERVICE",
-        instance_port=8083,
-        instance_ip="127.0.0.1",
-        prefer_ip_address=True
+        instance_port=PORT,
+        instance_host="localhost"
     )
 
 
